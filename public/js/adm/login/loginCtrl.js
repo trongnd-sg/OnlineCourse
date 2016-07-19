bceb6a0b26a067a4048c40a66e27d3bd4ab7cd21
Trong Nguyen <trong.nguyen@siglaz.vn>
2016-07-19 14:12:59 +0700

    Comment out login

--- public/js/adm/login/loginCtrl.js
+++ public/js/adm/login/loginCtrl.js
@@ -13,12 +13,12 @@ angular.module('courseApp.controllers')
 	$scope.signIn = function() {
 		AuthService.login($scope.userInfo).then(function(response) {
 			var user = AuthService.getUser()
-			if (user.role !== 'Admin') {
+			/*if (user.role !== 'Admin') {
 				AuthService.logout()
 				toastr.error('You have no permission!', 'Login Error', { closeButton: true, timeOut: 5000, showMethod: 'fadeIn',
 				hideMethod: 'fadeOut' })
 				return
-			}
+			}*/
 			$state.go('home')
 		}).catch(function(error) {
 			toastr.error(error.message.en, 'Login Error', { closeButton: true, timeOut: 5000, showMethod: 'fadeIn',	hideMethod: 'fadeOut' })
